ae4873e037e4793782747598990f1e2ffe675ebf
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-08 12:28:24 +0300

    Task_2

---
+++ Task_2.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите координаты точек в последовательности X, Y, Z: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int z1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+//Создаем функцию 
+double Decision(int x1, int x2, int y1, int y2, int z1, int z2)
+{
+    return Math.Sqrt(Math.Pow((x2 - x1), 2) +
+                     Math.Pow((y2 - y1), 2) +
+                     Math.Pow((z2 - z1), 2));
+}
+
+//Присваиваем значению Distance вызов функции с задаными координатами 
+double Distance = Decision(x1, x2, y1, y2, z1, z2);
+
+Console.WriteLine($"Расстояние между точками в 3D пространстве: {string.Format("{0:N2}", Distance)}");//отбрасываем символы после зяпятой 
