17e182125cfccfc8e4c230ca504653ee80aeb661
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-08 12:26:24 +0300

    Task_1

---
+++ Task_1.cs
@@ -0,0 +1,20 @@
+﻿
+Console.Write("Введите число: ");
+// принимаем число как строку
+string number = Console.ReadLine();
+
+void CheckNumber(string number)
+{
+    if (number[0] == number[4] || number[1] == number[3])
+    {
+        Console.WriteLine($"Ваше число: {number} - палиндром.");
+    }
+    else Console.WriteLine($"Ваше число: {number} - НЕ палиндром.");
+}
+
+// проверяем на соответствие пятизначному числу 
+if (number.Length == 5)
+{
+    CheckNumber(number);//вызываем функцию 
+}
+else Console.WriteLine($"Введи правильное число");
