33778d4f1431e3dcc2d6fd47f821fc26b6a8e4a5
Odarochka1802 <64232325+Odarochka1802@users.noreply.github.com>
2022-09-08 12:33:50 +0300

    Update Task_2.cs

--- Task_2.cs
+++ Task_2.cs
@@ -1,4 +1,8 @@
-﻿Console.WriteLine("Введите координаты точек в последовательности X, Y, Z: ");
+/*Задача 21: Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+A (3,6,8); B (2,1,-7), -> 15.84
+A (7,-5, 0); B (1,-1,9) -> 11.53
+*/
+Console.WriteLine("Введите координаты точек в последовательности X, Y, Z: ");
 int x1 = Convert.ToInt32(Console.ReadLine());
 int y1 = Convert.ToInt32(Console.ReadLine());
 int z1 = Convert.ToInt32(Console.ReadLine());
